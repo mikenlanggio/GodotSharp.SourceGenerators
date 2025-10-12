700e3a8e2523a8d69697dec3a0ce57256ff176a2
mikenlanggio <mikendaikgiangho@gmail.com>
2025-10-12 20:01:31 +0700

    Update Declarr keyword

--- SourceGenerators/AutoEnumExtensions/AutoEnumDataModel.cs
+++ SourceGenerators/AutoEnumExtensions/AutoEnumDataModel.cs
@@ -1,4 +1,6 @@
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace GodotSharp.SourceGenerators.AutoEnumExtensions;
 
@@ -6,6 +8,8 @@ internal class AutoEnumDataModel : ClassDataModel
 {
     public string IdentityProperty { get; } = "";
     public string[] EnumMembers { get; }
+
+    public string DeclarationKeyword { get; }
 
     public AutoEnumDataModel(INamedTypeSymbol symbol, string identityProperty = "")
         : base(symbol)
@@ -21,11 +25,22 @@ internal class AutoEnumDataModel : ClassDataModel
                 SymbolEqualityComparer.Default.Equals(f.Type, symbol))
             .Select(f => f.Name)
             .ToArray();
+
+        DeclarationKeyword = symbol.DeclaringSyntaxReferences
+            .Select(r => r.GetSyntax())
+            .OfType<TypeDeclarationSyntax>()
+            .Select(s => s.Kind() switch
+            {
+                SyntaxKind.RecordDeclaration => "record",
+                SyntaxKind.StructDeclaration => "struct",
+                _ => "class"
+            }).FirstOrDefault() ?? "class";
     }
 
     protected override string Str()
     {
         return string.Join("\n", EnumMembers.Select(x => $" - Member: {x}"))
-             + $"\nIdentityProperty: {IdentityProperty}";
+             + $"\nIdentityProperty: {IdentityProperty}"
+             + $"\nDeclarationKeyword: {DeclarationKeyword}";
     }
 }
